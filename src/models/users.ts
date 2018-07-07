b53932d4875853787b52a9ef1479e56f5155a732
Marei Kikukawa <contact@calmery.me>
2018-07-07 18:46:43 +0900

    Add models

---
+++ src/models/users.ts
@@ -0,0 +1,64 @@
+import { DataType, Table, Column, Model, AllowNull, PrimaryKey, HasMany } from 'sequelize-typescript'
+
+import Tweet from './tweets'
+import Mention from './mentions'
+
+@Table({
+  tableName: 'users',
+  paranoid: true
+})
+export default class User extends Model<User> {
+  @PrimaryKey
+	@Column
+	id: number
+
+  @AllowNull(false)
+	@Column
+	name: string
+
+  @AllowNull(false)
+	@Column
+	screen_name: string
+
+  @AllowNull(true)
+	@Column
+	description: string
+
+  @AllowNull(true)
+	@Column
+	url: string
+
+  @AllowNull(false)
+	@Column
+	followers_count: number
+
+  @AllowNull(false)
+	@Column
+	friends_count: number
+
+  @AllowNull(false)
+	@Column
+	statuses_count: number
+
+  @AllowNull(true)
+	@Column
+	profile_image_url: string
+
+  @AllowNull(true)
+	@Column
+	profile_background_image_url: string
+
+  @AllowNull(true)
+	@Column
+	profile_banner_url: string
+
+  @AllowNull(false)
+	@Column(DataType.DATE)
+  created_at: Date
+
+  @HasMany(() => Tweet)
+  tweets: Tweet[]
+
+  @HasMany(() => Mention)
+  mention: Mention[]
+}
