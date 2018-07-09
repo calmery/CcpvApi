7a911bf2198575a160a9010fc420115e24f35238
Marei Kikukawa <contact@calmery.me>
2018-07-09 16:06:48 +0900

    Remove db

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,6 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 
-import firebase from './firebase'
-
-import Sequelize from 'sequelize'
-import sequelize from './sequelize'
-import Accounts from './models/accounts'
-
 const app = express()
 
 app.use(cors())
@@ -93,64 +87,7 @@ app.post('/list', (request, response) => {
   })
 })
 
-app.post('/authentication', async (request, response) => {
-  const firebaseIdToken = request.body.firebase_id_token
-  const accessToken = request.body.access_token
-  const accessTokenSecret = request.body.access_token_secret
-
-  if (firebaseIdToken === undefined || accessToken === undefined || accessTokenSecret === undefined) {
-    response.status(400).end()
-  }
-
-  // Firebase に問い合わせる
-  const verified = await firebase.auth().verifyIdToken(firebaseIdToken)
-
-  const alreadyRegistered = await Accounts.find({
-    where: {
-      firebase_id: {
-        [Sequelize.Op.eq]: verified.uid
-      }
-    }
-  })
-
-  if (alreadyRegistered === null) {
-    try {
-      const user = Accounts.build({
-        name: verified.name,
-        firebase_id: verified.uid,
-        access_token: accessToken,
-        access_token_secret: accessTokenSecret
-      })
-
-      await user.save()
-
-      response.status(200).json({
-        id: user.getDataValue('id'),
-        name: user.getDataValue('name'),
-        api_key: user.getDataValue('api_key')
-      })
-    } catch (error) {
-      response.status(500).end()
-    }
-
-    return
-  }
-
-  response.status(200).json({
-    id: alreadyRegistered.getDataValue('id'),
-    name: alreadyRegistered.getDataValue('name'),
-    api_key: alreadyRegistered.getDataValue('api_key')
-  })
-})
-
 // Postgres との同期が完了してからサーバを起動する
-const _ = (async () => {
-  try {
-    await sequelize.sync()
-    app.listen(process.env.PORT || 8000)
-  } catch (error) {
-    throw error
-  }
-})()
+app.listen(process.env.PORT || 8000)
 
 export default app
--- src/sequelize.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize-typescript'
-import * as Constants from './constants'
-import Accounts from './models/accounts'
-
-const sequelize = new Sequelize(Constants.SEQUELIZE_CONFIG)
-
-sequelize.addModels([Accounts])
-
-export default sequelize
